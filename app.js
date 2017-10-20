1ddaddf213157010418550bdfe6baf88a6cd5237
BManiKanta001 <astrani_manikanta@outlook.com>
2017-10-20 23:54:19 +0530

    Added appId and Apppswd"
    Added appId and Apppswd

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: '',
-    appPassword: ''
+    appId: '27418aa4-1c80-4cdd-8487-0f7deb656d20',
+    appPassword: 'hxoaMpAR4T6O1XHokvft9hu'
 });
 
 // Listen for messages from users 

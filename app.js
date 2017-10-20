a374adc1804fc152a87c91dd9cae9fcc8c96a954
BManiKanta001 <astrani_manikanta@outlook.com>
2017-10-21 00:34:17 +0530

    For BackChannel sample
    For BackChannel sample

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
-var restify = require('restify');
-var builder = require('botbuilder');
+require('./connectorSetup.js')();
 
-// Setup Restify Server
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
-});
+//Bot listening for inbound backchannel events - in this case it only listens for events named "buttonClicked"
+bot.on("event", function (event) {
+    var msg = new builder.Message().address(event.address);
+    msg.textLocale("en-us");
+    if (event.name === "buttonClicked") {
+        msg.text("I see that you just pushed that button");
+    }
+    bot.send(msg);
+})
 
-// Create chat connector for communicating with the Bot Framework Service
-var connector = new builder.ChatConnector({
-    appId: '27418aa4-1c80-4cdd-8487-0f7deb656d20',
-    appPassword: 'hxoaMpAR4T6O1XHokvft9hu'
-});
- 
-// Listen for messages from users 
-server.post('/api/messages', connector.listen());
+//Basic root dialog which takes an inputted color and sends a changeBackground event. No NLP, regex, validation here - just grabs input and sends it back as an event. 
+bot.dialog('/', [
+    function (session) {
+        var reply = createEvent("changeBackground", session.message.text, session.message.address);
+        session.endDialog(reply);
+    }
+]);
 
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
-});
+//Creates a backchannel event
+const createEvent = (eventName, value, address) => {
+    var msg = new builder.Message().address(address);
+    msg.data.type = "event";
+    msg.data.name = eventName;
+    msg.data.value = value;
+    return msg;
+}
+
+
+
